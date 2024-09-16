34a9002868133fa3e21c3b952cb4409949d12e58
ratta <ratta@10.48.40.117>
2024-09-16 10:38:53 -0400

    NumCourses

---
+++ src/studentlist/part_time_student.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author ratta
+ */
+public class part_time_student extends Student {
+    private int NumCourses;
+
+    public int getNumCourses() {
+        return NumCourses;
+    }
+
+    public void setNumCourses(int NumCourses) {
+        this.NumCourses = NumCourses;
+    }
+    
+}
